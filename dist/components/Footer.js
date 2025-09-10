8432fffdd9d2e8cb7ac2c1b420ac723968a08e77
sukanta <sukanta@onehealthassist.com>
2025-09-10 17:11:38 +0530

    project complete

--- dist/components/Footer.js
+++ dist/components/Footer.js
@@ -1,3 +1,4 @@
+import React from 'react';
 export default function Footer() {
   return /*#__PURE__*/React.createElement(React.Fragment, null, "Footer");
 }
--- dist/components/Navbar.js
+++ dist/components/Navbar.js
@@ -1,3 +1,4 @@
+import React from 'react';
 export default function Navbar() {
   return /*#__PURE__*/React.createElement(React.Fragment, null, "navbar");
 }
